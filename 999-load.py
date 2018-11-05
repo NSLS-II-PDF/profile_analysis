c5c02fe4a88548d315077eec8623041f7b916e61
christopher <cjwright4242@gmail.com>
2018-11-05 14:58:44 -0500

    extra )

--- 999-load.py
+++ 999-load.py
@@ -9,6 +9,6 @@ def calib_from_fit2d(directDist, centerX, centerY,
                      pixelX=200, pixelY=200, splineFile=None):
     g = Geometry()
     g = g.setFit2D(directDist, centerX, centerY, tilt, tiltPlanRotation, 
-                 pixelX, pixelY, splineFile))
+                 pixelX, pixelY, splineFile)
     _save_calib_param(g, _timestampstr(time.time()),
                       os.path.join(glbl_dict['config_base'], glbl_dict['calib_config_name']))
