ab7ab03e460d09f64245953068483da3a96ceb5c
christopher <cjwright4242@gmail.com>
2018-11-05 14:19:25 -0500

    use setFit2D

--- 999-load.py
+++ 999-load.py
@@ -7,7 +7,8 @@ import time
 def calib_from_fit2d(directDist, centerX, centerY, 
                      tilt=0.0, tiltPlanRotation=0.0, 
                      pixelX=200, pixelY=200, splineFile=None):
-    g = Geometry(directDist, centerX, centerY, tilt, tiltPlanRotation, 
+    g = Geometry()
+    g = g.setFit2D(directDist, centerX, centerY, tilt, tiltPlanRotation, 
                  pixelX, pixelY, splineFile))
     _save_calib_param(g, _timestampstr(time.time()),
                       os.path.join(glbl_dict['config_base'], glbl_dict['calib_config_name']))
